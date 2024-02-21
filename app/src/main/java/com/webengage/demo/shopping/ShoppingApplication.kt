26760d11718a7d50477aaa47e4fa9fd1444c7aa7
haridas2021 <haridas.r@webengage.com>
2024-02-21 14:36:31 +0530

    integration

--- app/src/main/java/com/webengage/demo/shopping/ShoppingApplication.kt
+++ app/src/main/java/com/webengage/demo/shopping/ShoppingApplication.kt
@@ -2,12 +2,24 @@ package com.webengage.demo.shopping
 
 import android.app.Application
 import android.content.Context
+import com.webengage.sdk.android.WebEngageActivityLifeCycleCallbacks
+import com.webengage.sdk.android.WebEngageConfig
 
 class ShoppingApplication : Application() {
     private var mContext: Context? = null
     override fun onCreate() {
         super.onCreate()
         mContext = this.applicationContext
+        val webEngageConfig = WebEngageConfig.Builder()
+            .setWebEngageKey("YOUR_WEBENGAGE_LICENSE_CODE")
+            .setDebugMode(true) // only in development mode
+            .build()
+        registerActivityLifecycleCallbacks(
+            WebEngageActivityLifeCycleCallbacks(
+                this,
+                webEngageConfig
+            ))
+
     }
 
     fun getAppContext(): Context? {
