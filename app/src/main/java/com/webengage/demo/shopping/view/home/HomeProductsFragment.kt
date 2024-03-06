de7e4213f11451ca24aff58304bd3da93dd8168c
Sarthak <sarthak.sharma@weebklipper.com>
2024-03-06 19:33:53 +0530

    Product page rebuild with recycler view

--- app/src/main/java/com/webengage/demo/shopping/view/home/HomeProductsFragment.kt
+++ app/src/main/java/com/webengage/demo/shopping/view/home/HomeProductsFragment.kt
@@ -3,6 +3,7 @@ package com.webengage.demo.shopping.view.home
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,6 +19,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.webengage.demo.shopping.view.cart.CartViewModel
 import com.webengage.demo.shopping.view.productDetail.ProductDetailActivity
 import com.webengage.demo.shopping.R
+import com.webengage.personalization.WEPersonalization
+import com.webengage.personalization.callbacks.WECampaignCallback
+import com.webengage.personalization.callbacks.WEPlaceholderCallback
+import com.webengage.personalization.data.WECampaignData
 import kotlin.random.Random
 
 /**
@@ -25,7 +30,7 @@ import kotlin.random.Random
  * Use the [HomeProductsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeProductsFragment : Fragment() {
+class HomeProductsFragment : Fragment(), WEPlaceholderCallback, WECampaignCallback {
 
     private lateinit var clickedProduct: Product
     private var clickedCategoryIndex: Int = -1
@@ -51,6 +56,10 @@ class HomeProductsFragment : Fragment() {
             }
     }
 
+    override fun onStart() {
+        super.onStart()
+        WEPersonalization.get().registerWEPlaceholderCallback("we_placeholder_home", this)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
@@ -114,5 +123,48 @@ class HomeProductsFragment : Fragment() {
         val intent = Intent(context, ProductDetailActivity::class.java)
         intent.putExtra("product", product)
         productDetailActivityResultLauncher.launch(intent)
+    }
+
+    override fun onDataReceived(data: WECampaignData) {
+        Log.d("Tag", "mycallbacks onDataReceived ${data.targetViewId}, ${data.content!!.customData} ")
+        //Get the data for custom view and render your own UI by fetching data from the WECampaignData
+        //render the campaign on UI (own UI) and save the reference of WECampaignData attached with this UI,
+        //and when user clicks call the tackClick, as soon as yuo render call trackImpression
+    }
+
+    override fun onPlaceholderException(
+        campaignId: String?,
+        targetViewId: String,
+        error: Exception
+    ) {
+        Log.d("Tag", "mycallbacks onPlaceholderException $targetViewId, $error ")
+    }
+
+    override fun onRendered(data: WECampaignData) {
+        Log.d("Tag", "mycallbacks onRendered ${data.targetViewId}, ${data.content} ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        WEPersonalization.get().unregisterWECampaignCallback(this)
+    }
+
+    override fun onCampaignClicked(
+        actionId: String,
+        deepLink: String,
+        data: WECampaignData
+    ): Boolean {
+        return false
+    }
+
+    override fun onCampaignException(campaignId: String?, targetViewId: String, error: Exception) {
+    }
+
+    override fun onCampaignPrepared(data: WECampaignData): WECampaignData? {
+        return data
+    }
+
+    override fun onCampaignShown(data: WECampaignData) {
+
     }
 }
